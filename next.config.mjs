25f9889796ee5c1e2aa19cf66fa067c23ca4a134
phurin <le.phurin.work@gmail.com>
2025-03-25 00:07:21 +0700

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_CLIENT_VERSION: process.env.NEXT_PUBLIC_CLIENT_VERSION,
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    return config;
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
