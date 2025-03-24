0400a3da1e0601cbc25e18b35f52b9350557625a
phurin <le.phurin.work@gmail.com>
2025-03-24 23:54:23 +0700

    log clientVersion

--- src/components/pages/Home/page.jsx
+++ src/components/pages/Home/page.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export const Home = () => {
   const [serverVersion, setServerVersion] = useState();
+  const clientVersion = process.env.NEXT_PUBLIC_CLIENT_VERSION;
 
   const getServerVersion = async () => {
     const res = await fetch(`/api/version`);
@@ -13,6 +14,7 @@ export const Home = () => {
 
   useEffect(() => {
     getServerVersion();
+    console.log("clientVersion: ", clientVersion);
   }, []);
 
   return (
@@ -20,7 +22,7 @@ export const Home = () => {
       <div className="flex flex-col items-center justify-center p-14 bg-cyan-200 rounded-2xl">
         <h2 className="text-xl font-bold">To Test Deploy GCP with docker</h2>
         <hr className="my-4 h-[1px] bg-black w-full" />
-        <p>client version: {process.env.NEXT_PUBLIC_CLIENT_VERSION}</p>
+        <p>client version: {clientVersion}</p>
         <p>server version: {serverVersion}</p>
       </div>
     </div>
