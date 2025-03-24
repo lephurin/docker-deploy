a34236e918c3ebcb7fd2c690ad03fbfc3760f3d4
phurin <le.phurin.work@gmail.com>
2025-03-24 23:12:19 +0700

    update ui

--- src/components/pages/Home/page.jsx
+++ src/components/pages/Home/page.jsx
@@ -16,10 +16,13 @@ export const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h2 className="text-xl font-medium">To Test Deploy GCP with docker</h2>
-      <p>client version: {process.env.NEXT_PUBLIC_CLIENT_VERSION}</p>
-      <p>server version: {serverVersion}</p>
+    <div className="w-full min-h-screen flex justify-center items-center bg-cyan-800">
+      <div className="flex flex-col items-center justify-center p-14 bg-cyan-200 rounded-2xl">
+        <h2 className="text-xl font-bold">To Test Deploy GCP with docker</h2>
+        <hr className="my-4 h-[1px] bg-black w-full" />
+        <p>client version: {process.env.NEXT_PUBLIC_CLIENT_VERSION}</p>
+        <p>server version: {serverVersion}</p>
+      </div>
     </div>
   );
 };
