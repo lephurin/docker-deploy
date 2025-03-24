0d449cb505db3847576a522a11ac209ecc752e50
phurin <le.phurin.work@gmail.com>
2025-03-25 02:06:31 +0700

    install jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverage: true,
+  coverageReporters: ["text", "lcov"],
+  coverageDirectory: "coverage",
+  roots: ["<rootDir>/src", "<rootDir>/__tests__"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
