6a49f67fd8e80a5c7a796f429847709f020c59d5
phurin <le.phurin.work@gmail.com>
2025-04-03 22:03:03 +0700

    optional

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -20,7 +20,7 @@ export async function POST(request) {
       projectId: process.env.GCP_PROJECT_ID,
       credentials: {
         client_email: process.env.GCP_CLIENT_EMAIL,
-        private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, "\n"),
       },
     });
 
